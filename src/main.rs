4d3be73ce9d1e748559bccceebccfe730f6208d8
Tomás <tomas153lm@gmail.com>
2024-03-01 09:26:20 -0300

    basic programming concepts on rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{cmp::Ordering, io};
+use rand::Rng;
+
 fn main() {
-    println!("Hello world!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+
+        println!("Guess the number!");
+        println!("Please, input your guess.");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
 }
